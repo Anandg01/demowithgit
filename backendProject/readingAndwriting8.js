a4590e70752f95bac29b35a4f8f505461dda03a6
ptanadg01 <ptanandg@gmail.com>
2023-04-10 08:59:52 +0530

    redirect

--- backendProject/readingAndwriting8.js
+++ backendProject/readingAndwriting8.js
@@ -4,34 +4,38 @@ const fs=require('fs');
 const server =http.createServer((req, res)=>{
     const url=req.url;
     const method=req.method;
-   
-    {
+   console.log(url,method);
+
+   if(url==='/') {
    const text=fs.readFileSync('message.txt','utf-8')
-   console.log(text);
   res.write(`<html>`);
   res.write(`<head><title>Default page</title></head>`)
-  res.write(`<body> <p>${text}</p><form action="/message" method="POST"><input type="text" name="message">
-   <button type="submit">SEND</button></form></body>`)
+  res.write(`<body> <h3>${text}</h3><form action="/message" method="POST"><input type="text" name="message"> 
+  <button type="submit">SEND</button></form></body>`)
    res.write(`</html>`);
      res.end();
     }
 
 
-  if(method==='POST')  {
+ else if(url==='/message' && method==='POST')  {
     const body=[];
     req.on('data',(chunk)=>{
         console.log(chunk);
         body.push(chunk);
     })
 
-    req.on('end',()=>{
+  return req.on('end',()=>{
         const pbody=Buffer.concat(body).toString();
        const message=pbody.split('=')[1];
        console.log(message);
-        fs.writeFileSync('message.txt',message);
-        res.statusCode=303;
+
+        fs.writeFile('message.txt',message, (err)=>{
+          res.statusCode=303;
+          res.setHeader("Location",'/')
+          return res.end();  
+        } );
     })
-    return res.end();  
+
     }
 })
 
