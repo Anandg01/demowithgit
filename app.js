53f486b7dde377f8ed8f3ee494a3e268568daed0
ptanadg01 <ptanandg@gmail.com>
2023-04-12 12:47:32 +0530

    Intro to Middlewares add two input tag

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-const http=require('http');
+const http= require('http')
+const bodyParser=require('body-parser');
 
-const express=require('express')
-const app =express();
 
-app.use((req, res, next)=>{
-  console.log("1st middlewre ")
- 
+const express=require('express');
+const app =express()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/',(req, res, next)=>{
   next();
 })
 
-app.use((req, res, next)=>{
-    console.log("2nd middleware")
-    res.statusCode=302;
-    res.send(`<h1>Hello Express !</h1>`)
+app.use('/add-product',(req, res, next)=>{
+  res.statusCode=302;
+  res.send(`<form action="/product" method="POST"><input type="text" name="product_name" placeholder="Product Name">
+  <input type="number" name="price" placeholder="Enter Price">
+  <button type="submit">Send</button></form>`)
 })
-const server=http.createServer(app);
 
+app.post('/product',(req, res, next)=>{
+  console.log(req.body)
+  res.redirect('/add-product')
+})
+
+app.use('/',(req, res, next)=>{
+  res.send(`<h1>Hello World</h1>`)
+})
+
+const server=http.createServer(app);
 server.listen(3000, ()=>{
-    console.log('Server Running.......')
+  console.log("Server Running...")
 })
